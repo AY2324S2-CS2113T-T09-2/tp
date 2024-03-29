dd7ddf3587ec0d400c7d568175775402505cc2a0
nkotaa <e0957774@u.nus.edu>
2024-03-30 00:05:18 +0800

    Add Junit tests for SearchAssistant

--- src/main/java/seedu/binbash/inventory/SearchAssistant.java
+++ src/main/java/seedu/binbash/inventory/SearchAssistant.java
@@ -26,9 +26,6 @@ public class SearchAssistant {
     }
 
     public SearchAssistant searchByName(String nameField) {
-        if (foundItems.size() >= numberOfResults) {
-            return this;
-        }
         foundItems = (ArrayList<Item>) foundItems.stream()
             .filter(item -> item.getItemName().contains(nameField))
             .limit(numberOfResults)
@@ -37,20 +34,14 @@ public class SearchAssistant {
     }
 
     public SearchAssistant searchByDescription(String descriptionField) {
-        if (foundItems.size() >= numberOfResults) {
-            return this;
-        }
-        foundItems = (ArrayList<Item>) foundItems.stream()
+        foundItems = foundItems.stream()
             .filter(item -> item.getItemDescription().contains(descriptionField))
             .limit(numberOfResults)
-            .collect(Collectors.toList());
+            .collect(Collectors.toCollection(ArrayList::new));
         return this;
     }
 
     public SearchAssistant searchByCostPrice(double costPriceField) {
-        if (foundItems.size() >= numberOfResults) {
-            return this;
-        }
         foundItems = (ArrayList<Item>) foundItems.stream()
             .filter(item -> (costPriceField < 0) ? item.getItemCostPrice() < (-1 * costPriceField) : 
                     item.getItemCostPrice() > costPriceField)
@@ -60,9 +51,6 @@ public class SearchAssistant {
     }
 
     public SearchAssistant searchBySalePrice(double salePriceField) {
-        if (foundItems.size() >= numberOfResults) {
-            return this;
-        }
         foundItems = (ArrayList<Item>) foundItems.stream()
             .filter(item -> (salePriceField < 0) ?
                     item instanceof RetailItem && ((RetailItem) item).getItemSalePrice() < (-1 * salePriceField) :
@@ -74,14 +62,11 @@ public class SearchAssistant {
     }
 
     public SearchAssistant searchByExpiryDate(LocalDate expiryDateField) {
-        if (foundItems.size() >= numberOfResults) {
-            return this;
-        }
         foundItems = (ArrayList<Item>) foundItems.stream()
             .filter(item -> item instanceof PerishableRetailItem ?
                     LocalDate.parse(((PerishableRetailItem) item).getItemExpirationDate(),
                         DateTimeFormatter.ofPattern("dd-MM-yyyy"))
-                    .isBefore(expiryDateField) : true)
+                    .isBefore(expiryDateField) : false)
             .limit(numberOfResults)
             .collect(Collectors.toList());
         return this;
---
+++ src/test/java/seedu/binbash/inventory/SearchAssistantTest.java
@@ -0,0 +1,75 @@
+package seedu.binbash.inventory;
+
+import seedu.binbash.item.Item;
+import seedu.binbash.item.OperationalItem;
+import seedu.binbash.item.PerishableOperationalItem;
+import seedu.binbash.item.RetailItem;
+import seedu.binbash.item.PerishableRetailItem;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.ArrayList;
+import java.time.LocalDate;
+
+public class SearchAssistantTest {
+    private final SearchAssistant searchAssistant = new SearchAssistant();
+
+    @BeforeEach
+    public void setUp() {
+        ArrayList<Item> testItemList = new ArrayList<>();
+        testItemList.add(new OperationalItem("light bulb", "a light bulb used in the warehouse",
+                    100, 0.20));
+        testItemList.add(new PerishableOperationalItem("battery", "a battery for in-store use", 500,
+                    LocalDate.of(2024, 1, 16), 0.10));
+        testItemList.add(new RetailItem("black pen", "zebra sarasa black 0.5", 50, 1.00, 0.70));
+        testItemList.add(new PerishableRetailItem("banana", "cavendish banana", 30, LocalDate.of(2024, 1, 4), 0.50, 0.40));
+        testItemList.add(new PerishableRetailItem("milk", "meiji full fat whole milk", 10, LocalDate.of(2024, 2, 3), 5, 3.50));
+        searchAssistant.setFoundItems(testItemList);
+        searchAssistant.setNumberOfResults(testItemList.size());
+    }
+
+    @Test
+    public void searchByName_banana_success() {
+        ArrayList<Item> foundItems = searchAssistant.searchByName("banana")
+            .getFoundItems();
+        String foundItemName = foundItems.get(0).getItemName();
+        Assertions.assertTrue(foundItemName.contains("banana"));
+    }
+
+    @Test
+    public void searchByDescription_sarasa_success() {
+        ArrayList<Item> foundItems = searchAssistant.searchByDescription("sarasa")
+            .getFoundItems();
+        String foundItemDescription = foundItems.get(0).getItemDescription();
+        Assertions.assertTrue(foundItemDescription.contains("sarasa"));
+    }
+
+    @Test
+    public void searchByCostPrice_everythingMoreThan50Cents_foundPenAndMilk() {
+        ArrayList<Item> foundItems = searchAssistant.searchByCostPrice(0.50)
+            .getFoundItems();
+        String firstItemFound = foundItems.get(0).getItemName();
+        String secondItemFound = foundItems.get(1).getItemName();
+        Assertions.assertTrue(firstItemFound.equals("black pen") &&
+                secondItemFound.equals("milk") && foundItems.size() == 2);
+    }
+
+    @Test
+    public void searchBySalePrice_everythingLessThan60Cents_foundBanana() {
+        ArrayList<Item> foundItems = searchAssistant.searchBySalePrice(-0.60)
+            .getFoundItems();
+        String firstItemFound = foundItems.get(0).getItemName();
+        Assertions.assertTrue(firstItemFound.equals("banana") &&
+                foundItems.size() == 1);
+    }
+
+    @Test
+    public void searchByExpiryDate_everythingBefore20Jan_foundBanana() {
+        ArrayList<Item> foundItems = searchAssistant.searchByExpiryDate(LocalDate.of(2024, 1, 20))
+            .getFoundItems();
+        String firstItemFound = foundItems.get(0).getItemName();
+        Assertions.assertTrue(firstItemFound.equals("banana") && foundItems.size() == 1);
+    }
+}
