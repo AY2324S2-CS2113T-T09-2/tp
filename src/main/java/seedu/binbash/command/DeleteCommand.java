f8728a903bed11d04fc4dc16a89b45dbb335aa7e
PureUsagi <ngjunhancanoe@gmail.com>
2024-03-30 21:55:24 +0800

    Add JavaDocs to DeleteCommand class

--- src/main/java/seedu/binbash/command/DeleteCommand.java
+++ src/main/java/seedu/binbash/command/DeleteCommand.java
@@ -5,12 +5,20 @@ import java.util.logging.Level;
 import seedu.binbash.ItemList;
 import seedu.binbash.logger.BinBashLogger;
 
+/**
+ * Represents the execution of the delete command that will remove an item from the inventory.
+ */
 public class DeleteCommand extends Command {
     public static final Pattern COMMAND_FORMAT = Pattern.compile("delete\\s(?<identifier>.+)");
     private String keyword;
     private int index;
     private boolean isIndex;
 
+    /**
+     * Constructs a DeleteCommand object that will delete an item using item index.
+     *
+     * @param index index of the item to be deleted from the inventory.
+     */
     public DeleteCommand(int index) {
         this.index = index;
         isIndex = true;
@@ -21,6 +29,11 @@ public class DeleteCommand extends Command {
         ));
     }
 
+    /**
+     * Construsts a DeleteCommand object that will delete an item using item name.
+     *
+     * @param keyword a String matching the item name of the item to be deleted from the inventory.
+     */
     public DeleteCommand(String keyword) {
         this.keyword = keyword;
         isIndex = false;
@@ -30,6 +43,12 @@ public class DeleteCommand extends Command {
         ));
     }
 
+    /**
+     * Executes the delete command to remove an item from the inventory.
+     *
+     * @param itemList the inventory where the item will be deleted from.
+     * @return true if the command execution is successful, else false
+     */
     public boolean execute(ItemList itemList) {
         if (isIndex) {
             if (index <= 0 || index > itemList.getItemCount()) {
