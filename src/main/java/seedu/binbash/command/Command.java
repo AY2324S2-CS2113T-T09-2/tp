f6781329658ff67491c9c02faea8c15994ead962
PureUsagi <ngjunhancanoe@gmail.com>
2024-03-30 21:34:35 +0800

    Add JavaDocs to command abstract class

--- src/main/java/seedu/binbash/command/Command.java
+++ src/main/java/seedu/binbash/command/Command.java
@@ -3,6 +3,11 @@ package seedu.binbash.command;
 import java.util.logging.Logger;
 import seedu.binbash.ItemList;
 
+
+/**
+ * Represents the execution of a command entered by the user. Serves as an abstract to which
+ * all BinBash commands will inherit.
+ */
 public abstract class Command {
     protected ItemList itemList;
     protected Logger commandLogger;
@@ -13,13 +18,28 @@ public abstract class Command {
         commandLogger = Logger.getLogger("CommandLogger");
     }
 
+    /**
+     * Returns the outcome of the command execution that will be printed to the text UI.
+     * @return the outcome message as a String.
+     */
     public String getExecutionUiOutput() {
         return executionUiOutput;
     }
 
+    /**
+     * Returns whether the command executed will require BinBash to save its memory contents to the storage file.
+     * @return true if BinBash needs to save to storage file, else false
+     */
     public boolean hasToSave() {
         return hasToSave;
     }
 
+    /**
+     * Executes the command with parameters passed and stored prior to execution on the ItemList object
+     * passed as a parameter.
+     *
+     * @param itemList ItemList object that the command will execute on.
+     * @return True if command execution is successful, else False
+     */
     public abstract boolean execute(ItemList itemList);
 }
