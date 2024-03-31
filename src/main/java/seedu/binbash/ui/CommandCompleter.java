f05518452282dc5e85a34a36095bd64b64449d23
nkotaa <e0957774@u.nus.edu>
2024-03-31 12:21:08 +0800

    Refactor CommandCompleter

--- src/main/java/seedu/binbash/ui/CommandCompleter.java
+++ src/main/java/seedu/binbash/ui/CommandCompleter.java
@@ -17,23 +17,15 @@ import java.util.List;
 import java.util.Collection;
 import java.util.function.Function;
 
-public class CommandCompleter implements Completer {
+public class CommandCompleter extends AggregateCompleter {
     private static NullCompleter deleteOptionCompleter = NullCompleter.INSTANCE;
     private static NullCompleter listOptionCompleter = NullCompleter.INSTANCE;
     private static NullCompleter searchOptionCompleter = NullCompleter.INSTANCE;
     private static NullCompleter byeOptionCompleter = NullCompleter.INSTANCE;
-    //private static Function<String, Collection<OptDesc>> addCommandOptions = str -> {
-    //    return Arrays.asList(new OptDesc("-n", "--name", "item name", NullCompleter.INSTANCE),
-    //            new OptDesc("-q", "--quantity", "item quantity", new StringsCompleter()),
-    //            new OptDesc("-d", "--description", "item description", new StringsCompleter()),
-    //            new OptDesc("-e", "--expiry", "expiration date", new StringsCompleter()),
-    //            new OptDesc("-s", "--sale", "sale price", new StringsCompleter()),
-    //            new OptDesc("-c", "--cost", "cost price", new StringsCompleter()));
-    //};
 
-    private static Candidate addCommandCandidate = new Candidate("add", "<item name>", "general commands",
+    private static Candidate addCommandCandidate = new Candidate("add", "add", "inventory management",
             "add an item", "suffixadd", "keyadd", true);
-    private static ArgumentCompleter addCompleter;
+    private static ArgumentCompleter addCompleter = new ArgumentCompleter(new StringsCompleter(addCommandCandidate));
     private static ArgumentCompleter deleteCompleter = new ArgumentCompleter(new StringsCompleter("delete"),
             deleteOptionCompleter);
     private static ArgumentCompleter listCompleter = new ArgumentCompleter(new StringsCompleter("list"),
@@ -44,18 +36,10 @@ public class CommandCompleter implements Completer {
             byeOptionCompleter);
 
     private static OptionCompleter addOptionCompleter;
-    private final Collection<Completer> completers;
 
     public CommandCompleter(ArrayList<OptDesc> addCommandOptionDescriptions) {
-        addOptionCompleter = new OptionCompleter(Arrays.asList(NullCompleter.INSTANCE),
-                addCommandOptionDescriptions,
-                1);
-        addCompleter = new ArgumentCompleter(new StringsCompleter(addCommandCandidate), addOptionCompleter);
-        completers = Arrays.asList(addCompleter, deleteCompleter, listCompleter, searchCompleter, byeCompleter);
-    }
-
-    @Override
-    public void complete(LineReader reader, final ParsedLine line, final List<Candidate> candidates) {
-        completers.forEach(c -> c.complete(reader, line, candidates));
+        super(addCompleter, new StringsCompleter("search"));
+        addCompleter.getCompleters().add(new OptionCompleter(
+                    addCommandOptionDescriptions, 1));
     }
 }
