48db28b3235dd46a985aff4432ddf6df8e3f4755
PureUsagi <ngjunhancanoe@gmail.com>
2024-04-02 01:54:47 +0800

    Update comparator to  compare Items instead of RetailItems

--- src/main/java/seedu/binbash/comparators/ItemComparatorBySalePrice.java
+++ src/main/java/seedu/binbash/comparators/ItemComparatorBySalePrice.java
@@ -7,16 +7,22 @@ import seedu.binbash.item.RetailItem;
 
 import java.util.Comparator;
 
-public class ItemComparatorBySalePrice implements Comparator<RetailItem> {
+public class ItemComparatorBySalePrice implements Comparator<Item> {
+    private RetailItem retailItem1;
+    private RetailItem retailItem2;
+
     @Override
-    public int compare(RetailItem item1, RetailItem item2) {
-        if (item1.getItemSalePrice() < item2.getItemSalePrice()) {
+    public int compare(Item item1, Item item2) {
+        retailItem1 = (RetailItem) item1 ;
+        retailItem2 = (RetailItem) item2;
+
+        if (retailItem1.getItemSalePrice() < retailItem2.getItemSalePrice()) {
             return -1;
-        } else if (item1.getItemSalePrice() > item2.getItemCostPrice()) {
+        } else if (retailItem1.getItemSalePrice() > retailItem2.getItemCostPrice()) {
             return 1;
         }
 
-        return compareByPerishType(item1, item2);
+        return compareByPerishType(retailItem1, retailItem2);
     }
 
     private int compareByPerishType(RetailItem item1, RetailItem item2) {
