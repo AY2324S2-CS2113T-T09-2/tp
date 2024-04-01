88f9e7aff3e0becc533a12958eb94204d0b921ea
PureUsagi <ngjunhancanoe@gmail.com>
2024-04-02 00:19:28 +0800

    Update ListCommand class to to include option to print list sorted by expiry date

--- src/main/java/seedu/binbash/command/ListCommand.java
+++ src/main/java/seedu/binbash/command/ListCommand.java
@@ -5,14 +5,26 @@ import seedu.binbash.logger.BinBashLogger;
 
 
 public class ListCommand extends Command {
-
+    private String sortType;
     public ListCommand() {
         commandLogger = new BinBashLogger(ListCommand.class.getName());
         commandLogger.info("Creating List Command...");
+        this.sortType =  "none";
+    }
+
+    public ListCommand(String sortType) {
+        commandLogger = new BinBashLogger(ListCommand.class.getName());
+        commandLogger.info("Creating List Command with sort type of expiry date...");
+        this.sortType = sortType;
     }
 
     public boolean execute(ItemList itemList) {
-        executionUiOutput = itemList.printList(itemList.getItemList());
+        if (sortType.equals("e")) {
+            executionUiOutput = itemList.printListSortedByExpiryDate(itemList.getItemList());
+        } else {
+            executionUiOutput = itemList.printList(itemList.getItemList());
+        }
+
         return true;
     }
 }
