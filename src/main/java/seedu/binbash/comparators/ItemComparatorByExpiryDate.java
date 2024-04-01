569f3d33d50ca92e56367bcaf84d0d56c6e13116
PureUsagi <ngjunhancanoe@gmail.com>
2024-04-02 02:33:05 +0800

    Add JavaDocs for ItemCompoaratorByExpiryDate class

--- src/main/java/seedu/binbash/comparators/ItemComparatorByExpiryDate.java
+++ src/main/java/seedu/binbash/comparators/ItemComparatorByExpiryDate.java
@@ -6,14 +6,25 @@ import seedu.binbash.item.PerishableRetailItem;
 
 import java.util.Comparator;
 
-
+/**
+ * Represents a comparator used to sort items based on their expiry date.
+ */
 public class ItemComparatorByExpiryDate implements Comparator<Item> {
     private PerishableOperationalItem perishOp1;
     private PerishableOperationalItem perishOp2;
     private PerishableRetailItem perishRe1;
     private PerishableRetailItem perishRe2;
 
-    @Override
+    /**
+     * Compares two objects of the Item class based on their expiry date.
+     * First condition - Item with the earlier expiry date will be placed before item with the later expiry date.
+     * Second condition - Retail items will be placed before operational items.
+     * Third condition - Item with lower quantity will be placed before items with higher quantity.
+     *
+     * @param item1 the first object to be compared.
+     * @param item2 the second object to be compared.
+     * @return compare results.
+     */
     public int compare(Item item1, Item item2) {
         if (item1 instanceof PerishableOperationalItem && item2 instanceof PerishableOperationalItem) {
             perishOp1 = (PerishableOperationalItem) item1 ;
@@ -34,6 +45,15 @@ public class ItemComparatorByExpiryDate implements Comparator<Item> {
         }
     }
 
+    /**
+     * Compares two objects of the PerishableOperationalItem class based on their expiry date.
+     * First condition - Item with the earlier expiry date will be placed before item with the later expiry date.
+     * Second condition - Item with lower quantity will be placed before item with higher quantity.
+     *
+     * @param item1 the first object to be compared.
+     * @param item2 the second object to be compared.
+     * @return compare results.
+     */
     private int compareOpToOp (PerishableOperationalItem item1, PerishableOperationalItem item2) {
         if (item1.getItemExpirationDateLocalDate().equals(item2.getItemExpirationDateLocalDate())) {
             return compareQty(item1.getItemQuantity(), item2.getItemQuantity());
@@ -44,6 +64,16 @@ public class ItemComparatorByExpiryDate implements Comparator<Item> {
         return 1;
     }
 
+    /**
+     * Compares objects of the PerishableOperationalItem and PerishableRetailItem classes based on their expiry date.
+     * First condition - Items with the earlier expiry date will be placed before item with the later expiry date.
+     * Second condition - PerishableRetailItems will be placed before PerishableOperationalItems.
+     * Second condition - Item with lower quantity will be placed before item with higher quantity.
+     *
+     * @param item1 the first object to be compared.
+     * @param item2 the second object to be compared.
+     * @return compare results.
+     */
     private int compareOpToRe (PerishableOperationalItem item1, PerishableRetailItem item2) {
         if (item1.getItemExpirationDateLocalDate().equals(item2.getItemExpirationDateLocalDate())) {
             return 1;
@@ -54,6 +84,15 @@ public class ItemComparatorByExpiryDate implements Comparator<Item> {
         return 1;
     }
 
+    /**
+     * Compares objects of the PerishableRetailItem class based on their expiry date.
+     * First condition - Item with earlier expiry date will be placed before item with later expiry date.
+     * Second condition - Item with lower quantity will be placed before item with higher quantity.
+     *
+     * @param item1 the first object to be compared.
+     * @param item2 the second object to be compared.
+     * @return compare results.
+     */
     private int compareReToRe (PerishableRetailItem item1, PerishableRetailItem item2) {
         if (item1.getItemExpirationDateLocalDate().equals(item2.getItemExpirationDateLocalDate())) {
             return compareQty(item1.getItemQuantity(), item2.getItemQuantity());
@@ -64,6 +103,16 @@ public class ItemComparatorByExpiryDate implements Comparator<Item> {
         return 1;
     }
 
+    /**
+     * Compares objects of the PerishableOperationalItem and PerishableRetailItem classes based on their expiry date.
+     * First condition - Items with the earlier expiry date will be placed before item with the later expiry date.
+     * Second condition - PerishableRetailItems will be placed before PerishableOperationalItems.
+     * Second condition - Item with lower quantity will be placed before item with higher quantity.
+     *
+     * @param item1 the first object to be compared.
+     * @param item2 the second object to be compared.
+     * @return compare results.
+     */
     private int compareReToOp (PerishableRetailItem item1, PerishableOperationalItem item2) {
         if (item1.getItemExpirationDateLocalDate().equals(item2.getItemExpirationDateLocalDate())) {
             return -1;
@@ -74,6 +123,13 @@ public class ItemComparatorByExpiryDate implements Comparator<Item> {
         return 1;
     }
 
+    /**
+     * Compares the value of two integers based. The lower quantity will be placed before the higher quantity.
+     *
+     * @param qty1 the quantity of the first item.
+     * @param qty2 the quantity of the second item.
+     * @return compare results.
+     */
     private int compareQty(int qty1, int qty2) {
         if (qty1 < qty2) {
             return -1;
