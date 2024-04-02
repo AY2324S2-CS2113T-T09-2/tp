eed0a0f5e575e539c7806ae2454accef80d5ab82
PureUsagi <ngjunhancanoe@gmail.com>
2024-04-02 11:45:15 +0800

    Add JavaDocs to ListCommand class

--- src/main/java/seedu/binbash/command/ListCommand.java
+++ src/main/java/seedu/binbash/command/ListCommand.java
@@ -3,21 +3,38 @@ package seedu.binbash.command;
 import seedu.binbash.inventory.ItemList;
 import seedu.binbash.logger.BinBashLogger;
 
-
+/**
+ * Represents the execution of the list command that will display the inventory in a certain order.
+ */
 public class ListCommand extends Command {
     private String sortType;
+
+    /**
+     * Constructs a ListCommand object that will print the unsorted inventory.
+     */
     public ListCommand() {
         commandLogger = new BinBashLogger(ListCommand.class.getName());
         commandLogger.info("Creating List Command...");
         this.sortType =  "none";
     }
 
+    /**
+     * Constructs a ListCommand object that will print the sorted inventory based on sort option.
+     *
+     * @param sortType sorting method used to sort the inventory.
+     */
     public ListCommand(String sortType) {
         commandLogger = new BinBashLogger(ListCommand.class.getName());
         commandLogger.info("Creating List Command with sort type " + sortType + "...");
         this.sortType = sortType;
     }
 
+    /**
+     * Executes the list command to print the inventory in a certain order.
+     *
+     * @param itemList ItemList object that the command will execute on.
+     * @return true if the command execution is successful.
+     */
     public boolean execute(ItemList itemList) {
         switch(sortType) {
         case "e":
