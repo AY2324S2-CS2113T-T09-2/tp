3a95b7a68caf3a5c368152d9358b03728e4a4230
nkotaa <e0957774@u.nus.edu>
2024-04-05 23:02:05 +0800

    SearchAssistant.java: Add JavaDoc

--- src/main/java/seedu/binbash/inventory/SearchAssistant.java
+++ src/main/java/seedu/binbash/inventory/SearchAssistant.java
@@ -10,9 +10,16 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
+/**
+ * A search assistant with a number of helper methods to search through an item list
+ * based on an arbitrary number of fields.
+ */
 public class SearchAssistant {
     private ArrayList<Item> foundItems;
 
+    /**
+     * Note that the list of items to search through has to be explicity set by the caller.
+     */
     public void setFoundItems(ArrayList<Item> foundItems) {
         this.foundItems = foundItems;
     }
@@ -21,6 +28,13 @@ public class SearchAssistant {
         return foundItems;
     }
 
+    /**
+     * Returns at most the desired number of items found
+     *
+     * @param numberOfResults The number of results desired
+     * @return A list of the first numberOfResults items in foundItems if at least numberOfResults items
+     * in foundItems, the full foundItems list otherwise
+     */
     public ArrayList<Item> getFoundItems(int numberOfResults) {
         if (numberOfResults > foundItems.size()) {
             return foundItems;
@@ -28,6 +42,12 @@ public class SearchAssistant {
         return new ArrayList<Item>(foundItems.subList(0, numberOfResults));
     }
 
+    /**
+     * Finds all items whose names contain a given string.
+     *
+     * @param nameField The given string.
+     * @return The current instance of SearchAssistant.
+     */
     public SearchAssistant searchByName(String nameField) {
         if (nameField.equals("")) {
             return this;
@@ -38,6 +58,12 @@ public class SearchAssistant {
         return this;
     }
 
+    /**
+     * Finds all items that contain a given string in their description.
+     *
+     * @param descriptionField The given string.
+     * @return The current instance of SearchAssistant.
+     */
     public SearchAssistant searchByDescription(String descriptionField) {
         if (descriptionField.equals("")) {
             return this;
@@ -48,12 +74,25 @@ public class SearchAssistant {
         return this;
     }
 
+    /**
+     * Finds all items with quantities between a certain range, inclusive.
+     *
+     * @param from The lower bound of this range.
+     * @param to The upper bound of this range.
+     * @return The current instance of SearchAssistant.
+     */
     public SearchAssistant searchByQuantityBetween(int from, int to) {
         searchByQuantityFrom(from);
         searchByQuantityTo(to);
         return this;
     }
 
+    /**
+     * Finds all items whose quantities are equal to or above a given value.
+     *
+     * @param from The given price.
+     * @return The current instance of SearchAssistant.
+     */
     public SearchAssistant searchByQuantityFrom(int from) {
         if (from < 0) {
             return this;
@@ -64,6 +103,12 @@ public class SearchAssistant {
         return this;
     }
 
+    /**
+     * Finds all items whose quantities are equal to or below a given price.
+     *
+     * @param to The given price.
+     * @return The current instance of SearchAssistant.
+     */
     public SearchAssistant searchByQuantityTo(int to) {
         if (to == Integer.MAX_VALUE) {
             return this;
@@ -74,12 +119,25 @@ public class SearchAssistant {
         return this;
     }
 
+    /**
+     * Finds all items that cost between $<from> and $<to>, inclusive.
+     *
+     * @param from The lower bound of the range.
+     * @param to The upper bound of the range.
+     * @return The current instance of SearchAssistant.
+     */
     public SearchAssistant searchByCostPriceBetween(double from, double to) {
         searchByCostPriceFrom(from);
         searchByCostPriceTo(to);
         return this;
     }
 
+    /**
+     * Finds all items that cost as much as or more than a given price.
+     *
+     * @param fromPrice The given price.
+     * @return The current instance of SearchAssistant.
+     */
     public SearchAssistant searchByCostPriceFrom(double fromPrice) {
         if (fromPrice == Double.MIN_VALUE) {
             return this;
@@ -90,6 +148,12 @@ public class SearchAssistant {
         return this;
     }
 
+    /**
+     * Finds all items that cost as much as or less than a given price.
+     *
+     * @param toPrice The given price.
+     * @return The current instance of SearchAssistant.
+     */
     public SearchAssistant searchByCostPriceTo(double toPrice) {
         if (toPrice == Double.MAX_VALUE) {
             return this;
@@ -100,12 +164,26 @@ public class SearchAssistant {
         return this;
     }
 
+    /**
+     * Finds all items that sell for between $<from> and $<to>, inclusive.
+     *
+     * @param from The lower bound of the range.
+     * @param to The upper bound of the range.
+     * @return The current instance of SearchAssistant.
+     */
     public SearchAssistant searchBySalePriceBetween(double from, double to) {
         searchBySalePriceFrom(from);
         searchBySalePriceTo(to);
         return this;
     }
 
+    /**
+     * Finds all items that sell for as much or more than a given price.
+     * Note that items without a sale price are not considered to match this query, and will be filtered out.
+     *
+     * @param fromPrice The given price.
+     * @return The current instance of SearchAssistant.
+     */
     public SearchAssistant searchBySalePriceFrom(double fromPrice) {
         if (fromPrice == Double.MIN_VALUE) {
             return this;
@@ -117,6 +195,13 @@ public class SearchAssistant {
         return this;
     }
 
+    /**
+     * Finds all items that sell for as much or less than a given price.
+     * Note that items without a sale price are not considered to match this query, and will be filtered out.
+     *
+     * @param toPrice The given price.
+     * @return The current instance of SearchAssistant.
+     */
     public SearchAssistant searchBySalePriceTo(double toPrice) {
         if (toPrice == Double.MAX_VALUE) {
             return this;
@@ -128,12 +213,26 @@ public class SearchAssistant {
         return this;
     }
 
+    /**
+     * Finds all items that expire between two dates, inclusive.
+     *
+     * @param from The start date.
+     * @param to The end date.
+     * @return The current instance of SearchAssistant.
+     */
     public SearchAssistant searchByExpiryDateBetween(LocalDate from, LocalDate to) {
         searchByExpiryDateFrom(from);
         searchByExpiryDateTo(to);
         return this;
     }
 
+    /**
+     * Finds all items that expire on or after a given date.
+     * Note that items without an expiry date are not considered to match this query, and will be filtered out.
+     *
+     * @param fromDate The given date.
+     * @return The current instance of SearchAssistant.
+     */
     public SearchAssistant searchByExpiryDateFrom(LocalDate fromDate) {
         if (fromDate == LocalDate.MIN) {
             return this;
@@ -149,6 +248,13 @@ public class SearchAssistant {
         return this;
     }
 
+    /**
+     * Finds all items that expire on or before a given date.
+     * Note that items without an expiry date are not considered to match this query, and will be filtered out.
+     *
+     * @param toDate The given date.
+     * @return The current instance of SearchAssistant.
+     */
     public SearchAssistant searchByExpiryDateTo(LocalDate toDate) {
         if (toDate == LocalDate.MAX) {
             return this;
