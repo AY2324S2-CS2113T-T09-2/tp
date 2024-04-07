a75c4bb456fd0da3921d18f529f42ff38cf62f87
XavierLiau34 <xavyliau@gmail.com>
2024-04-07 23:58:30 +0800

    Solve Diff Error

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -14,7 +14,20 @@ rm -rf data log
 java -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt &> ACTUAL.TXT
 
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix EXPECTED-UNIX.TXT ACTUAL.TXT
+
+# Check the operating system and convert line endings accordingly
+if [[ $(uname -s) == "Darwin" ]]; then
+    # macOS
+    sed -i '' 's/\r$//' EXPECTED-UNIX.TXT ACTUAL.TXT
+elif [[ $(uname -s) == "Linux" ]]; then
+    # Linux
+    dos2unix EXPECTED-UNIX.TXT ACTUAL.TXT
+elif [[ $(uname -s) == "CYGWIN"* || $(uname -s) == "MINGW"* ]]; then
+    # Windows
+    unix2dos -n EXPECTED-UNIX.TXT EXPECTED-UNIX-WINDOWS.TXT
+    unix2dos -n ACTUAL.TXT ACTUAL-WINDOWS.TXT
+fi
+
 # compare the twenty-third line, after quote output onwards
 diff <(tail -n +23 ACTUAL.TXT) EXPECTED.TXT
 if [ $? -eq 0 ]
