b4b36c4537b7d7bdfdf22084e1b32880713e64d5
XavierLiau34 <98327898+XavierLiau34@users.noreply.github.com>
2024-04-08 18:18:22 +0800

    Update runtest.bat

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -20,4 +20,8 @@ java -jar %jarloc% < ..\..\text-ui-test\input.txt > ..\..\text-ui-test\ACTUAL.TX
 
 cd ..\..\text-ui-test
 
-FC ACTUAL.TXT EXPECTED.TXT >NUL && ECHO Test passed! || Echo Test failed!
+REM Exclude line 19 from both ACTUAL.TXT and EXPECTED.TXT before comparison
+findstr /v /n "^19:" ACTUAL.TXT > ACTUAL_TEMP.TXT
+findstr /v /n "^19:" EXPECTED.TXT > EXPECTED_TEMP.TXT
+
+FC ACTUAL_TEMP.TXT EXPECTED_TEMP.TXT 
