766f0ebcd054ab11548edfa0a48db02f8a6fa584
nkotaa <e0957774@u.nus.edu>
2024-04-08 19:19:02 +0800

    Move quantity test to parser

--- src/test/java/seedu/binbash/command/SellCommandTest.java
+++ src/test/java/seedu/binbash/command/SellCommandTest.java
@@ -37,15 +37,6 @@ public class SellCommandTest {
     }
 
     @Test
-    public void sellItem_returnsErrorMessage_negativeQuantity() {
-        ItemList itemList = new ItemList(new ArrayList<>());
-        itemList.addItem("retail", "Test Item", "Test Description", 10, LocalDate.MIN, 5.0, 2.0, 5);
-        SellCommand command = new SellCommand("Test Item", -5);
-        command.execute(itemList);
-        assertEquals("Please provide a positive number.", command.getExecutionUiOutput());
-    }
-
-    @Test
     public void sellItem_decreasesQuantity_validIndexAndQuantity() {
         ItemList itemList = new ItemList(new ArrayList<>());
         itemList.addItem("retail", "Test Item", "Test Description", 10, LocalDate.MIN, 5.0, 2.0, 5);
--- src/test/java/seedu/binbash/parser/SellCommandParserTest.java
+++ src/test/java/seedu/binbash/parser/SellCommandParserTest.java
@@ -5,15 +5,15 @@ import org.junit.jupiter.api.Assertions;
 
 import org.apache.commons.cli.ParseException;
 
-public class RestockCommandParserTest {
-    private final RestockCommandParser restockCommandParser = new RestockCommandParser();
+public class SellCommandParserTest {
+    private final SellCommandParser sellCommandParser = new SellCommandParser();
 
     @Test
     public void parse_negativeQuantity_showsNegativeQuantityWarning() {
         String[] invalidCommandArgs = new String[]{"-i", "3", "-q", "-5"};
         ParseException thrown = Assertions.assertThrows(
                 ParseException.class, () -> {
-                    restockCommandParser.parse(invalidCommandArgs);
+                    sellCommandParser.parse(invalidCommandArgs);
                 }, "ParseException was expected");
         Assertions.assertEquals(thrown.getMessage(), "Please provide a positive number.");
     }
