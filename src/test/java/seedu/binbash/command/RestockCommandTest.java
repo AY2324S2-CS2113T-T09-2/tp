9746f5db4c6e7ffebed8489ad079c81359bea899
nkotaa <e0957774@u.nus.edu>
2024-04-08 19:10:53 +0800

    Move negative quantity tests to Parser
    Our parser now handles the check for negative quantities, meaning
    RestockCommand is guaranteed to be given a positive quantity. Thus there
    is no need for such a test.

--- src/test/java/seedu/binbash/command/RestockCommandTest.java
+++ src/test/java/seedu/binbash/command/RestockCommandTest.java
@@ -37,15 +37,6 @@ public class RestockCommandTest {
     }
 
     @Test
-    public void restockItem_returnsErrorMessage_negativeQuantity() {
-        ItemList itemList = new ItemList(new ArrayList<>());
-        itemList.addItem("retail", "Test Item", "Test Description", 10, LocalDate.MIN, 5.0, 2.0, 5);
-        RestockCommand command = new RestockCommand("Test Item", -5);
-        command.execute(itemList);
-        assertEquals("Please provide a positive number.", command.getExecutionUiOutput());
-    }
-
-    @Test
     public void restockItem_increasesQuantity_validIndexAndQuantity() {
         ItemList itemList = new ItemList(new ArrayList<>());
         itemList.addItem("retail", "Test Item", "Test Description", 10, LocalDate.MIN, 5.0, 2.0, 5);
