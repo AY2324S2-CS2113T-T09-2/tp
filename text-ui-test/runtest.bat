9fd8eab36082dbd22680ab52903896b218b4a9b4
XavierLiau34 <98327898+XavierLiau34@users.noreply.github.com>
2024-04-08 16:37:39 +0800

    Update runtest.bat

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -20,8 +20,8 @@ java -jar %jarloc% < ..\..\text-ui-test\input.txt > ..\..\text-ui-test\ACTUAL.TX
 
 cd ..\..\text-ui-test
 
-REM Delete line 19 from ACTUAL.TXT
-type ACTUAL.TXT | findstr /v /n "^19:" > ACTUAL_TEMP.TXT
+REM Delete contents of line 19 in ACTUAL.TXT and replace it with a blank line
+for /f "tokens=1,* delims=:" %%A in ('findstr /n "^" ACTUAL.TXT ^| findstr /v /b "19:"') do echo(%%B > ACTUAL_TEMP.TXT
 
 REM Compare ACTUAL_TEMP.TXT with EXPECTED.TXT
 FC ACTUAL_TEMP.TXT EXPECTED.TXT
