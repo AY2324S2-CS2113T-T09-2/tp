a88c45cc58030b8fe3c286eb7f389ffe509b5b2e
PureUsagi <ngjunhancanoe@gmail.com>
2024-04-13 15:55:34 +0800

    Refactor DeleteCommandParserTest to use test setup

--- src/test/java/seedu/binbash/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/binbash/parser/DeleteCommandParserTest.java
@@ -2,17 +2,24 @@ package seedu.binbash.parser;
 
 import org.apache.commons.cli.ParseException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import seedu.binbash.command.DeleteCommand;
 
 class DeleteCommandParserTest {
+    DeleteCommandParser deleteCommandParser;
+    DeleteCommand deleteCommand;
+
+    @BeforeEach
+    void setUp() {
+        deleteCommandParser = new DeleteCommandParser();
+    }
 
     @Test
     void parse_deleteByIndexOption_success() {
         String[] invalidCommandArgs = new String[]{"-i", "1"};
-        DeleteCommandParser deleteCommandParser = new DeleteCommandParser();
         try {
-            DeleteCommand deleteCommand = deleteCommandParser.parse(invalidCommandArgs);
+            deleteCommand = deleteCommandParser.parse(invalidCommandArgs);
             Assertions.assertTrue(deleteCommand instanceof DeleteCommand);
         } catch (ParseException e) {
             Assertions.fail();
@@ -22,9 +29,8 @@ class DeleteCommandParserTest {
     @Test
     void parse_deleteByNameOption_success() {
         String[] invalidCommandArgs = new String[]{"-n", "1"};
-        DeleteCommandParser deleteCommandParser = new DeleteCommandParser();
         try {
-            DeleteCommand deleteCommand = deleteCommandParser.parse(invalidCommandArgs);
+            deleteCommand = deleteCommandParser.parse(invalidCommandArgs);
             Assertions.assertTrue(deleteCommand instanceof DeleteCommand);
         } catch (ParseException e) {
             Assertions.fail();
